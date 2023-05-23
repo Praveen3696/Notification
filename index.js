dc37ec75c17377070fafa5062dd9427d4580bd3c
Praveen <praveenkaniti@gmail.com>
2023-05-23 06:05:50 +0000

    notifi_2

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 const Notification = (props) => {
-  //  Write your code here.
-  const {className,name,src} = props
-  const containerClassName = {`notification-cont ${className}`}
-  return(
-      <div className = {containerClassName}>
-          <img className = "icon" src = {src}/>
-          <p className = "para">{name}</p>
-      </div>
-  )
+  const { className, iconUrl, message } = props;
+  const containerClassName = `notification-container ${className}`;
+
+  return (
+    <div className={containerClassName}>
+      <img className="icon" src={iconUrl} />
+      <p className="message">{message}</p>
+    </div>
+  );
 };
 
 const element = (
-  //  Write your code here.
-  <div className="bg-container">
-    <h1 className="container_head">Notifications</h1>
-    <div className="notification-cont">
-      <div className="blue_container">
-       <Notification img src="https://assets.ccbp.in/frontend/react-js/primary-icon-img.png" className = "icon"/>
-       <Notification p className = "para" name= "Information Message"/>
-      </div>
-
-      <div className="green_container">
-       <Notification img src="https://assets.ccbp.in/frontend/react-js/success-icon-img.png" className = "icon"/>
-       <Notification p className = "para" name= "Success Message"/>
-      </div>
-
-      <div className="yellow_container">
-        <Notification img src="https://assets.ccbp.in/frontend/react-js/warning-icon-img.png" className = "icon"/>
-       <Notification p className = "para" name= "Warning Message"/>
-      </div>
-
-      <div className="red_container">
-        <Notification img src="https://assets.ccbp.in/frontend/react-js/danger-icon-img.png" className = "icon"/>
-       <Notification p className = "para" name= "Error Message"/>
-      </div>
+  <div className="notifications-app-container">
+    <h1 className="title">Notifications</h1>
+    <div className="notifications-list-container">
+      <Notification
+        className="primary-bg-color"
+        iconUrl="https://assets.ccbp.in/frontend/react-js/primary-icon-img.png"
+        message="Information Message"
+      />
+      <Notification
+        className="success-bg-color"
+        iconUrl="https://assets.ccbp.in/frontend/react-js/success-icon-img.png"
+        message="Success Message"
+      />
+      <Notification
+        className="warning-bg-color"
+        iconUrl="https://assets.ccbp.in/frontend/react-js/warning-icon-img.png"
+        message="Warning Message"
+      />
+      <Notification
+        className="danger-bg-color"
+        iconUrl="https://assets.ccbp.in/frontend/react-js/danger-icon-img.png"
+        message="Error Message"
+      />
     </div>
   </div>
 );
